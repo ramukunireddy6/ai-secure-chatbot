3d9e32e5a722de7c26698312796900067ba74e39
ramukunireddy <ramukunireddy6@gmail.com>
2026-01-27 11:01:36 -0500

    guardrails as service

--- chatbot-api/main.py
+++ chatbot-api/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException
-from schema import ChatRequest
 from auth.oidc import validate_token
+from guardrails.pre_guard import  pre_guard_check
+from guardrails.post_guard import post_guard_check
+from agents.google_adk_agent import run_agent
+
+app = FastAPI()
+
+@app.post("/chat")
+async def chat(request: dict,user = Depends(validate_token)):
+    prompt = request["query"]
+
+    response = await run_agent(prompt,user)
+
+    post_guard_check(response)
+
+    return  {"response": response}
 
 
